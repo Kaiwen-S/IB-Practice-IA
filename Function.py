0c2b49d98ed058afcbb092b5c8bb453a3c39049a
Kevin Song <swkkevin@hotmail.com>
2021-03-01 14:49:37 +0800

    Function

--- Function.py
+++ Function.py
@@ -26,4 +26,15 @@ def Search(arr,x,Method):
     elif Method=='Binary':
         BinarySearch(arr,x)
 
-# Make sure to type 'from Search import *' to import all the functions defined in this file
+# Make sure to type 'from Funcion import *' to import all the functions defined in this file
+
+def sorting_alphabet(movies_arr):
+    for i in range(len(movies_arr)):
+        m=movies_arr[i]
+        for j in range(i+1,len(movies_arr)):
+            if m>movies_arr[j]:
+                m=movies_arr[j]
+                max_index=j
+                movies_arr[i],movies_arr[j]=movies_arr[j],movies_arr[i]
+    for i in range(len(movies_arr)):
+        print(movies_arr[i])
