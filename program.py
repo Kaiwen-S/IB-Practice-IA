040688ba597f307b1ba4cf2b9958b5054e9881e7
Cici Hou <75721477+longearblu@users.noreply.github.com>
2021-03-04 18:25:59 +0800

    Update program.py

--- program.py
+++ program.py
@@ -8,14 +8,21 @@ print("[sort] for sorting the movies")
 print("[search] for searching if the movie exixts")
 print("[quit] to exit")
 
+movielist=[]
+
 loop = True #enter the loop
 chance = 3 #number of invalid commands permitted
 while loop == True:
     c=input("Type in your anticipated action:")
 
     if c == "list":
-        print(FileIntoList("database.txt"))
-
+        print(movielist)
+    
+    if c == "edit":
+        edit_c=input("If you want to add, type [A}; if you want to delete, type [D}.")
+        if edit_c=="A":
+            
+        if edit_c=="D":
     else:
         print("Your command is invalid, please try again.\n You have " + n + " more chances.")
         if chance==0:
